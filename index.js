d50a50c0e65c33a419ca9d857dc3dd428b85acc6
Mikhail <mixael.poladof@mail.ru>
2026-05-09 17:04:20 +0300

    fixed static files

--- index.js
+++ index.js
@@ -151,6 +151,6 @@ app.post('/submit', async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
+app.listen(3000, '0.0.0.0', () => {
     console.log('Server started on http://localhost:3000');
 });
