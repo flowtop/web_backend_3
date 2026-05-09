c23df3da8a85a90814f21320393bf4c70aa6115e
Mikhail <mixael.poladof@mail.ru>
2026-05-09 16:15:20 +0300

    static files fixed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("static"));
+app.use(express.static(__dirname + "static"));
 
 // Подключение к БД
 const pool = mysql.createPool({
